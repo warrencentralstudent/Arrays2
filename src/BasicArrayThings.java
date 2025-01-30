1a6744649aa5474cae539e80678e925bbcd730b1
Davian Affognon <affogdav000@warren.k12.in.us>
2025-01-30 12:03:36 -0500

    okay

--- src/BasicArrayThings.java
+++ src/BasicArrayThings.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class BasicArrayThings {
+public class BasicArrayThings {
+
+    public static void main(String[] args) {
+        int[] array = {87, 93, 12, 25, 36};
+
+
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+
+            }
+        }
+        System.out.println("Max: " + max);
+    }
 }
+
